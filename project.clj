df4727eaa93322ce0c5a907cbbfb6e1058d97574
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2026-09-17 13:46:27 -0300

    fixing project.clj file

--- project.clj
+++ project.clj
@@ -1,7 +1,35 @@
-(defproject health-check "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://example.com/FIXME"
+(defproject net.clojars.macielti/health-check "0.1.0"
+  :description "Health check component for clojure backend applications"
+
+  :url "https://github.com/macielti/health-check"
+
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
-  :repl-options {:init-ns health-check.core})
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.12.6"]]
+
+  :profiles {:dev {:resource-paths ^:replace ["test/resources"]
+
+                   :test-paths     ^:replace ["test/unit" "test/integration" "test/helpers"]
+
+                   :plugins        [[com.github.clojure-lsp/lein-clojure-lsp "2.1.0"]
+                                    [com.github.liquidz/antq "RELEASE"]]
+
+                   :dependencies   [[prismatic/schema "1.4.2"]
+                                    [nubank/matcher-combinators "3.11.0"]
+                                    [hashp "0.2.2"]]
+
+                   :injections     [(require 'hashp.core)]
+
+                   :aliases        {"clean-ns"     ["clojure-lsp" "clean-ns" "--dry"] ;; check if namespaces are clean
+                                    "format"       ["clojure-lsp" "format" "--dry"] ;; check if namespaces are formatted
+                                    "diagnostics"  ["clojure-lsp" "diagnostics"]
+                                    "lint"         ["do" ["clean-ns"] ["format"] ["diagnostics"]]
+                                    "clean-ns-fix" ["clojure-lsp" "clean-ns"]
+                                    "format-fix"   ["clojure-lsp" "format"]
+                                    "lint-fix"     ["do" ["clean-ns-fix"] ["format-fix"]]}
+
+                   :repl-options   {:init-ns health-check.component}
+                   }
+             }
+  )
